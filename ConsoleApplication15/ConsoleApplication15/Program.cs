015fc69d5d645ac86f6551d2b683cda663fce735
Coffeeks <101855266+Coffeeks@users.noreply.github.com>
2022-03-18 12:53:17 +0800

    Add files via upload

---
+++ ConsoleApplication15/ConsoleApplication15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Дано целое число n больше 0. Найти сумму 1 + 1/2 + 1/3 + ... + 1/n");
+            double S = 0;
+            double i, n;
+            n = Int32.Parse(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                S = S + (1 / i);
+            }
+            Console.WriteLine("Ответ: " + S);
+            Console.ReadKey();
+        }
+    }
+}
