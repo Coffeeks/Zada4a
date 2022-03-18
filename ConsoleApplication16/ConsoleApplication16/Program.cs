282a0b355df71769b5119d90bfc3ed1ce671b25d
Coffeeks <101855266+Coffeeks@users.noreply.github.com>
2022-03-18 13:15:21 +0800

    Add files via upload

---
+++ ConsoleApplication16/ConsoleApplication16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("S =  n^2 + (n + 1)^2 + (n + 2)^2 + ... + (2n)^2");
+            double S = 0, n, i;
+            n = Int32.Parse(Console.ReadLine());
+            for (i = 0; i <= n; i++)
+            {
+                S = S + Math.Pow(n + i,2);
+            }
+            Console.WriteLine("Ответ: " + S);
+            Console.ReadKey();
+        }
+    }
+}
